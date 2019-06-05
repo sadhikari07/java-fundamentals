820372284c8ce952a8542ed046f02f1e1d90e3d1
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-06-05 15:41:53 -0700

    revised review

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -2,38 +2,20 @@ package inheritance;
 
 public class Review extends ReviewAll {
 
-    private Restaurant restaurant;
-    private Shop shop;
-    private Theater theater;
     private String movieWatched;
 
-//Following is review constructor for restaurant
-    public Review(String author, String body, int stars, Restaurant restaurant){
-        super(author, body, stars);
-        this.restaurant = restaurant;
-    }
-
-    //Following is review constructor for Shop
-    public Review(String author, String body, int stars, Shop shop){
-        super(author, body, stars);
-        this.shop = shop;
-    }
-
-    //Following is review constructor for theater with name of movie
-    public Review(String author, String body, int stars, Theater theater, String movieWatched){
-        super(author, body, stars);
-        this.theater = theater;
+    public Review(String author, String body, int stars, Business business, String movieWatched){
+        super(author, body, stars, business);
         this.movieWatched = movieWatched;
     }
 
     //Following is review constructor for theater without the name of the movie
-    public Review(String author, String body, int stars, Theater theater){
-        super(author, body, stars);
-        this.theater = theater;
+    public Review(String author, String body, int stars, Business business){
+        super(author, body, stars, business);
     }
 
     public String toString(){
-        return String.format("%s has posted following review: %s, and has given %d rating", this.getAuthor(), this.getBody(), this.getStars());
+        return String.format("%s has posted following review for %s: %s, and has given %d rating", this.getAuthor(), this.getBusiness().getName(), this.getBody(), this.getStars());
     }
 
 }
--- inheritance/src/test/java/inheritance/ReviewTest.java
+++ inheritance/src/test/java/inheritance/ReviewTest.java
@@ -11,9 +11,9 @@ public class ReviewTest {
 
     @Test
     public void testConstructorRestaurantReview() {
-        Restaurant subway = new Restaurant("subway", 3, "$");
+        Restaurant subway = new Restaurant("Subway", 3, "$");
         Review michael = new Review("Michael Scott", "Really crappy restaurant", 1, subway);
-        assertEquals("Michael Scott has posted following review: Really crappy restaurant, and has given 1 rating", michael.toString());
+        assertEquals("Michael Scott has posted following review for Subway: Really crappy restaurant, and has given 1 rating", michael.toString());
     }
 
 
@@ -21,7 +21,7 @@ public class ReviewTest {
     public void testConstructorShopReview() {
         Shop walmart = new Shop("Walmart", "We sell cheap stuff.", "$");
         Review michael = new Review("Michael Scott", "I dont like it", 1, walmart);
-        assertEquals("Michael Scott has posted following review: I dont like it, and has given 1 rating", michael.toString());
+        assertEquals("Michael Scott has posted following review for Walmart: I dont like it, and has given 1 rating", michael.toString());
     }
 
     @Test
@@ -30,7 +30,7 @@ public class ReviewTest {
         moviesPlaying.add("Xmen");
         Theater regal = new Theater("Regal", moviesPlaying);
         Review michael = new Review("Michael Scott", "Bad movie", 1, regal);
-        assertEquals("Michael Scott has posted following review: Bad movie, and has given 1 rating", michael.toString());
+        assertEquals("Michael Scott has posted following review for Regal: Bad movie, and has given 1 rating", michael.toString());
     }
 
     @Test
@@ -39,7 +39,7 @@ public class ReviewTest {
         moviesPlaying.add("Xmen");
         Theater regal = new Theater("Regal", moviesPlaying);
         Review michael = new Review("Michael Scott", "Bad movie", 1, regal, "Xmen");
-        assertEquals("Michael Scott has posted following review: Bad movie, and has given 1 rating", michael.toString());
+        assertEquals("Michael Scott has posted following review for Regal: Bad movie, and has given 1 rating", michael.toString());
     }
 
 
